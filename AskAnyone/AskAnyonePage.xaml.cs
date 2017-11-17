53c13d507d7082f9a739a0b25edb60d8573687f0
MitchellHodzen <MitchellHodzen@yahoo.com>
2017-11-17 16:01:02 -0500

    Started function to add new question

--- AskAnyone/AskAnyonePage.xaml.cs
+++ AskAnyone/AskAnyonePage.xaml.cs
@@ -6,26 +6,46 @@ namespace AskAnyone
 {
     public partial class AskAnyonePage : TabbedPage
     {
-
+        List<Button> questions = new List<Button>();
         public AskAnyonePage()
         {
             InitializeComponent();
 
-            DefaultQuestion.Text = "Is pluto a planet?\nNo, pluto is not a planet.";
-            DefaultQuestion.Image = "icecream.png";
+            SetUpQuestion(DefaultQuestion1, "Is pluto a planet?\nNo, pluto is not a planet");
+            questions.Add(DefaultQuestion1);
+            SetUpQuestion(DefaultQuestion2, "Make Money\nFuck Bitches");
+            questions.Add(DefaultQuestion2);
+            questions.Add(HiddenQuestion1);
+
+        }
+
+        void SetUpQuestion(Button button, String text)
+        {
+            button.Text = text;
+            button.Image = "icecream.png";
+        }
+
+        void InsertNewQuestion(string questionText)
+        {
+            for(int i = questions.Count - 1; i > 0; i--)
+            {
+                SetUpQuestion(questions[i], questions[i - 1].Text);
+            }
+            SetUpQuestion(questions[0], questionText);
         }
 
         void Handle_NewQuestionClicked(object sender, System.EventArgs e)
         {
             try
             {
+                InsertNewQuestion("test");
                 //push the NewQuestionPage to the front
                 Navigation.PushAsync(new AskNewQuestionPage());
             }
             catch (Exception ex)
             {
                 //display error if caught
-                DisplayAlert("Exception", ex.Message, "Ok", "Cancel");
+                DisplayAlert("Exception on New Question:", ex.Message, "Ok", "Cancel");
             }
         }
 
@@ -48,7 +68,7 @@ namespace AskAnyone
             catch (Exception ex)
             {
                 //display error if caught
-                DisplayAlert("Exception", ex.Message, "Ok", "Cancel");
+                DisplayAlert("Exception on Question Button:", ex.Message, "Ok", "Cancel");
             }
         }
     }
